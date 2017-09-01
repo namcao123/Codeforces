ae24553d4ef515a1f9e5f33d774dab3241f51e67
Nam Nguyen <mse.nguyennhunam@gmail.com>
2017-09-01 16:41:14 -0400

    Update 217A.cpp

--- 217A.cpp
+++ 217A.cpp
@@ -1,30 +1,29 @@
 #include<iostream>
+using namespace std;
 
 long x[102], y[102], i, j, k, n, a;
 bool u[102];
 
 //function to check visited point
-void f(long long aa)
+void f(long aa)
 {
     long z;
     u[aa] = true;
     for (z = 0; z < n; z++)
-    {       //!u[z] mean u[z] = false;
+          //!u[z] mean u[z] = false;
             //if point is not visited and meet the condition
             if (!u[z] && (x[z] == x[aa] || y[z] == y[aa]))
             f(z);
-    }
-    
 }
-using namespace std;
 
 int main()
 {
-    int n ; cin >> n;
-    for (int i = 0; i < n; i++)
-        cin >> x[i] >> y[i];
+    cin >> n;
+    for(i=0;i<n;i++)
+    scanf("%ld %ld",&x[i],&y[i]);
+    
     for (i = 0; i < n; i++)
-    if (!u[i])
+     if (!u[i])
     {
         a++;
         f(i);
