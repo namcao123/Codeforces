e7f390f54e42c9149e8b2c7fd5643c1516c26688
Nam Nguyen <mse.nguyennhunam@gmail.com>
2017-09-11 22:23:28 -0400

    675a - math

---
+++ 675a.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+long long a, b, c;
+
+
+int main()
+{
+    cin >> a >> b >> c;
+
+    if(c == 0)
+    {
+      if (a == b) cout << "YES";
+      else cout << "NO";
+    }
+
+    else
+    {
+      if ((b-a)%c == 0 && (b-a)/c >=0 ) cout << "YES";
+    else cout << "NO";
+    }
+
+    return 0;
+}
